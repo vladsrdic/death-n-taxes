5f37ef5fc0f883caa94cce199ceb708d04c386f5
Vladimir Srdic <vlad.srdic@outlook.com>
2023-02-26 17:05:46 -0700

    Inventory, potions, and menu handlers

--- game_map.py
+++ game_map.py
@@ -5,7 +5,7 @@ from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np  # type: ignore
 from tcod.console import Console
 
-from entity import Actor
+from entity import Actor, Item
 import tile_types
 
 if TYPE_CHECKING:
@@ -29,7 +29,7 @@ class GameMap:
     @property
     def gamemap(self) -> GameMap:
         return self
-    
+
     @property
     def actors(self) -> Iterator[Actor]:
         """Iterate over this maps living actors."""
@@ -38,6 +38,10 @@ class GameMap:
             for entity in self.entities
             if isinstance(entity, Actor) and entity.is_alive
         )
+
+    @property
+    def items(self) -> Iterator[Item]:
+        yield from (entity for entity in self.entities if isinstance(entity, Item))
 
     def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
         for entity in self.entities:
