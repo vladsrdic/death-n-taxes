f1ae94c6846d17108e95cbebcd8229efec4f746b
Vladimir Srdic <vlad.srdic@outlook.com>
2023-03-13 18:42:16 -0600

    Create gui subfolder

--- engine.py
+++ engine.py
@@ -8,8 +8,8 @@ from tcod.console import Console
 from tcod.map import compute_fov
 
 import exceptions
-from message_log import MessageLog
-import render_functions
+from gui.message_log import MessageLog
+import gui.render_functions as render_functions
 
 if TYPE_CHECKING:
     from entity import Actor
